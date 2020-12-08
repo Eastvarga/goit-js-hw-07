5978eb77a04400b6b50bef09d63f79591e14ae25
Alexander Serzhenko <alexsergspectr@gmail.com>
2020-12-08 16:16:44 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,11 @@ const inputNameRef =  document.querySelector('input#name-input');
 // console.dir(inputNameRef);
 const nameOutputRef = document.querySelector('span#name-output');
 // console.dir(nameOutputRef);
-inputNameRef.addEventListener('input',(event)=>{
+const nameOutputer = (event)=>{
     if(event.target.value ===''){
     nameOutputRef.textContent = 'незнакомец';
     } else 
     nameOutputRef.textContent = event.target.value;
-});
+}
+
+inputNameRef.addEventListener('input',nameOutputer);
