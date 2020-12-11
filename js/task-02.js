fbba95c8796b982c0fd8b4ba87c4cf64348576a9
Alexander Serzhenko <alexsergspectr@gmail.com>
2020-12-11 12:54:08 +0200

    Task 02 Correction

--- js/task-02.js
+++ js/task-02.js
@@ -6,10 +6,16 @@ const ingredients = [
   "Зелень",
   "Приправы",
 ];
-const newElementsArray = [ingredients.length];
-for (const elem of ingredients) {
-  newElementsArray[ingredients.indexOf(elem)] = document.createElement("li");
-  newElementsArray[ingredients.indexOf(elem)].textContent = elem;
-}
+const newElementsArray = ingredients.map((element) => {
+  const newElement = document.createElement("li");
+  newElement.textContent = element;
+  return newElement;
+});
+// [ingredients.length];
+
+// for (const elem of ingredients) {
+//   newElementsArray[ingredients.indexOf(elem)] = document.createElement("li");
+//   newElementsArray[ingredients.indexOf(elem)].textContent = elem;
+// }
 //   console.dir(newElementsArray);
-document.querySelector("ul#ingredients").append(...newElementsArray);
+document.querySelector("#ingredients").append(...newElementsArray);
