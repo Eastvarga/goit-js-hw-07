66c72adb59600b66919f604e23a3bc0b96a874bb
Alexander Serzhenko <alexsergspectr@gmail.com>
2020-12-11 17:38:54 +0200

    Task 08 Correction

--- js/task-08.js
+++ js/task-08.js
@@ -2,19 +2,15 @@ const btnRenderRef = document.querySelector('button[data-action="render"');
 // console.dir(btnRenderRef);
 const btnDestroyRef = document.querySelector('button[data-action="destroy"');
 // console.dir(btnDestroyRef);
-const countElementsRef = document.querySelector("div#controls > input");
+const countElementsRef = document.querySelector("#controls > input");
 // console.dir(countElementsRef);
-const canvasBoxRef = document.querySelector("div#boxes");
+const canvasBoxRef = document.querySelector("#boxes");
 // console.dir(canvasBoxRef);
 
-function randomFnc() {
-  return Math.floor(Math.random() * 256);
-}
-
 function randomColorFnc() {
-  const r = randomFnc();
-  const g = randomFnc();
-  const b = randomFnc();
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
   return `rgb(${r},${g},${b})`;
 }
 // console.log(randomColorFnc());
@@ -23,10 +19,10 @@ function createBoxes(amount) {
   if (amount == 0) {
     return;
   }
-  const boxArray = [amount];
+  const boxArray = [];
   const size = 30;
   for (let i = 0; i < amount; i += 1) {
-    boxArray[i] = document.createElement("div");
+    boxArray.push(document.createElement("div"));
     boxArray[i].style.width = boxArray[i].style.height = `${size + i * 10}px`;
     boxArray[i].style.display = "block";
     boxArray[i].style.backgroundColor = randomColorFnc();
@@ -39,6 +35,8 @@ function destroyBoxes() {
   while (canvasBoxRef.firstChild) {
     canvasBoxRef.removeChild(canvasBoxRef.firstChild);
   }
+  canvasBoxRef.innerHTML = "";
+  countElementsRef.value = "";
 }
 
 btnRenderRef.addEventListener("click", () => {
