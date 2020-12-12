0d86dd1f9798a0d12aa4ff01e720cc49c4273cab
Alexander Serzhenko <alexsergspectr@gmail.com>
2020-12-12 22:59:42 +0200

    Update task 05 correction

--- js/task-05.js
+++ js/task-05.js
@@ -2,12 +2,24 @@ const inputNameRef = document.querySelector("#name-input");
 // console.dir(inputNameRef);
 const nameOutputRef = document.querySelector("#name-output");
 // console.dir(nameOutputRef);
-const nameOutputHandler = (event) => {
-  if (event.target.value.length !== 0) {
-    nameOutputRef.textContent = event.target.value;
-    return;
-  }
-  nameOutputRef.textContent = "незнакомец";
-};
 
-inputNameRef.addEventListener("input", nameOutputHandler);
+// const nameOutputHandler = (event) => {
+//   if (event.target.value.length !== 0) {
+//     nameOutputRef.textContent = event.target.value;
+//     return;
+//   }
+//   nameOutputRef.textContent = "незнакомец";
+// };
+
+// inputNameRef.addEventListener("input", nameOutputHandler);
+
+nameOutputRef.hidden = true;
+const checkUp = () => {
+  let userInfo = inputNameRef.value.trim();
+  nameOutputRef.hidden = false;
+  inputNameRef.value.length === 0
+    ? (nameOutputRef.textContent = "незнакомец")
+    : (nameOutputRef.textContent = userInfo);
+};
+inputNameRef.addEventListener("input", checkUp);
+inputNameRef.addEventListener("blur", checkUp);
